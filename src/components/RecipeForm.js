931c0cff4ab8d668e5fb7414142e01e71aed14e2
luigibustos <louisalphonsobustos@gmail.com>
2023-01-30 15:47:01 -0800

    Created Recipe Class and Create Form

---
+++ src/components/RecipeForm.js
@@ -0,0 +1,137 @@
+const RecipeForm = ({ recipeForm, handleChange, handleSubmit }) => {
+  return (
+    <form
+      className="w-full py-32 sm:py-48 grid place-items-center gap-2 text-center sm:text-left"
+      onSubmit={handleSubmit}
+    >
+      <div className="form-item mt-4">
+        <label htmlFor="recipeName" className="form-label">
+          Recipe Name
+        </label>
+        <input
+          className="form-field"
+          placeholder="Recipe Name"
+          type="text"
+          name="recipeName"
+          access="false"
+          id="recipeName"
+          onChange={handleChange}
+          value={recipeForm.recipeName}
+        />
+      </div>
+      <div className="form-item">
+        <label htmlFor="roast" className="form-label">
+          Roast
+        </label>
+        <select
+          name="roast"
+          id="roast"
+          className="form-field"
+          onChange={handleChange}
+          value={recipeForm.roast}
+        >
+          <option disabled value="Select Roast">
+            Select Roast
+          </option>
+          <option value="light" id="roast-0">
+            Light Roast
+          </option>
+          <option value="medium" id="roast-1">
+            Medium Roast
+          </option>
+          <option value="dark" id="roast-2">
+            Dark Roast
+          </option>
+        </select>
+      </div>
+      <div className="form-item">
+        <label htmlFor="size" className="form-label">
+          Serving Size
+        </label>
+        <select
+          name="size"
+          id="size"
+          className="form-field"
+          onChange={handleChange}
+          value={recipeForm.size}
+        >
+          <option disabled value="Select Serving Size">
+            Select Serving Size
+          </option>
+          <option value={225} id="size-0">
+            Single
+          </option>
+          <option value={450} id="size-1">
+            Double
+          </option>
+          <option value={300} id="size-2">
+            Tetsu
+          </option>
+        </select>
+      </div>
+      <div className="form-item">
+        <label htmlFor="balance" className="form-label">
+          Balance
+        </label>
+        <select
+          name="balance"
+          id="balance"
+          className="form-field"
+          onChange={handleChange}
+          value={recipeForm.balance}
+        >
+          <option disabled value="Select Balance">
+            Select Balance
+          </option>
+          <option value="standard" id="balance-0">
+            Standard
+          </option>
+          <option value="sweeter" id="balance-1">
+            Sweeter
+          </option>
+          <option value="brighter" id="balance-2">
+            Brighter
+          </option>
+        </select>
+      </div>
+      <div className="form-item">
+        <label htmlFor="strength" className="form-label">
+          Strength
+        </label>
+        <select
+          name="strength"
+          id="strength"
+          className="form-field"
+          onChange={handleChange}
+          value={recipeForm.strength}
+        >
+          <option disabled value="Select Strength">
+            Select Strength
+          </option>
+          <option value="light" id="strength-0">
+            Light
+          </option>
+          <option value="medium" id="strength-1">
+            Medium
+          </option>
+          <option value="strong" id="strength-2">
+            Strong
+          </option>
+        </select>
+      </div>
+      <div>
+        <button
+          className="form-btn hover:bg-blue-700"
+          type="submit"
+          name="button-1675104395386"
+          access="false"
+          id="button-1675104395386"
+        >
+          Submit
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default RecipeForm;
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,108 +1,117 @@
+import { useState } from "react";
+import RecipeForm from "../components/RecipeForm";
 const Create = () => {
+  const [recipeForm, setRecipeForm] = useState({
+    recipeName: "",
+    roast: "Select Roast",
+    size: "Select Serving Size",
+    balance: "Select Balance",
+    strength: "Select Strength",
+  });
+
+  class Recipe {
+    constructor(recipeName, roast, size, balance, strength) {
+      this.recipeName = recipeName;
+      this.roast = roast;
+      this.balance = balance;
+      this.strength = strength;
+      this.size = size;
+      this.servingSize =
+        size === 225 ? "Single" : size === 450 ? "Double" : "Tetsu";
+      this.waterTemp =
+        roast === "light" ? "200" : roast === "medium" ? "190" : "180";
+      this.waterAmount = size;
+      this.coffeeAmount = (size * 0.0733).toFixed(1);
+      this.pours = {};
+    }
+
+    calcBalance() {
+      switch (this.balance) {
+        case "standard":
+          this.pours.firstPour = (this.size * 0.2).toString();
+          this.pours.secondPour = (this.size * 0.2).toString();
+          break;
+        case "sweeter":
+          this.pours.firstPour = (this.size * (50 / 3) * 0.01)
+            .toFixed(1)
+            .toString();
+          this.pours.secondPour = (this.size * (70 / 3) * 0.01)
+            .toFixed(1)
+            .toString();
+          break;
+        case "brighter":
+          this.pours.firstPour = (this.size * (70 / 3) * 0.01)
+            .toFixed(1)
+            .toString();
+          this.pours.secondPour = (this.size * (50 / 3) * 0.01)
+            .toFixed(1)
+            .toString();
+          break;
+        default:
+          console.error(
+            `Invalid balance: ${this.balance}. Allowed values are 'standard', 'sweeter', and 'brighter'`
+          );
+          break;
+      }
+      return this;
+    }
+
+    calcStrength() {
+      switch (this.strength) {
+        case "light":
+          this.pours.thirdPour = (this.size * 0.6).toString();
+          break;
+        case "medium":
+          this.pours.thirdPour = (this.size * 0.3).toString();
+          this.pours.fourthPour = (this.size * 0.3).toString();
+          break;
+        case "strong":
+          this.pours.thirdPour = (this.size * 0.2).toString();
+          this.pours.fourthPour = (this.size * 0.2).toString();
+          this.pours.fifthPour = (this.size * 0.2).toString();
+          break;
+        default:
+          console.error(
+            `Invalid strength: ${this.strength}. Allowed values are 'light', 'medium', and 'strong'`
+          );
+      }
+      return this;
+    }
+  }
+
+  const handleChange = (e) => {
+    setRecipeForm({ ...recipeForm, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const currentState = { ...recipeForm };
+    console.log("currentState", currentState);
+    const newRecipe = new Recipe(
+      currentState.recipeName,
+      currentState.roast,
+      currentState.size,
+      currentState.balance,
+      currentState.strength
+    );
+    newRecipe.calcBalance().calcStrength();
+    console.log(newRecipe);
+    setRecipeForm({
+      recipeName: "",
+      roast: "Select Roast",
+      size: "Select Serving Size",
+      balance: "Select Balance",
+      strength: "Select Strength",
+    });
+  };
+
   return (
     <div className="max-h-screen w-screen pt-14 bg-neutral-100">
-      <div className="w-full py-32 grid place-items-center gap-2 text-center sm:text-left">
-        <div className="form-item mt-4">
-          <label htmlFor="recipeName" className="form-label">
-            Recipe Name
-          </label>
-          <input
-            className="form-field"
-            type="text"
-            placeholder="Recipe Name"
-            name="recipeName"
-            access="false"
-            id="recipeName"
-          />
-        </div>
-        <div className="form-item">
-          <label htmlFor="roast" className="form-label">
-            Roast
-          </label>
-          <select name="roast" id="roast" className="form-field">
-            <option disabled="null" defaultValue="null">
-              Roast
-            </option>
-            <option value="light" id="roast-0">
-              Light Roast
-            </option>
-            <option value="medium" id="roast-1">
-              Medium Roast
-            </option>
-            <option value="dark" id="roast-2">
-              Dark Roast
-            </option>
-          </select>
-        </div>
-        <div className="form-item">
-          <label htmlFor="size" className="form-label">
-            Serving Size
-          </label>
-          <select name="size" id="size" className="form-field">
-            <option disabled="null" defaultValue="null">
-              Serving Size
-            </option>
-            <option value="225" id="size-0">
-              Single
-            </option>
-            <option value="450" id="size-1">
-              Double
-            </option>
-            <option value="300" id="size-2">
-              Tetsu
-            </option>
-          </select>
-        </div>
-        <div className="form-item">
-          <label htmlFor="balance" className="form-label">
-            Balance
-          </label>
-          <select name="balance" id="balance" className="form-field">
-            <option disabled="null" defaultValue="null">
-              Balance
-            </option>
-            <option value="standard" id="balance-0">
-              Standard
-            </option>
-            <option value="sweeter" id="balance-1">
-              Sweeter
-            </option>
-            <option value="brighter" id="balance-2">
-              Brighter
-            </option>
-          </select>
-        </div>
-        <div className="form-item">
-          <label htmlFor="strength" className="form-label">
-            Strength
-          </label>
-          <select name="strength" id="strength" className="form-field">
-            <option disabled="null" defaultValue="null">
-              Strength
-            </option>
-            <option value="light" id="strength-0">
-              Light
-            </option>
-            <option value="medium" id="strength-1">
-              Medium
-            </option>
-            <option value="strong" id="strength-2">
-              Strong
-            </option>
-          </select>
-        </div>
-        <div>
-          <button
-            className="form-btn hover:bg-blue-700"
-            type="submit"
-            name="button-1675104395386"
-            access="false"
-            id="button-1675104395386"
-          >
-            Submit
-          </button>
-        </div>
-      </div>
+      <RecipeForm
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+        recipeForm={recipeForm}
+      />
     </div>
   );
 };
